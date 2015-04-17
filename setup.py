73e6661ab6d226638c80aa824bea09a38667e376
George Vlahavas <vlahavas@gmail.com>
2015-04-17 16:04:48 +0300

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# vim:et:sta:sts=4:sw=4:ts=8:tw=79:
+
+# setup.py Install script for simpleconfig.py
+# Copyright (C) 2011 George Vlahavas
+# E-mail: vlahavas ~ at ~ gmail ~ dot ~ com
+
+# This software is licensed under the terms of the GPLv3 license.
+
+from setuptools import setup, find_packages
+ 
+setup (
+    name = "FunctionPlot",
+    version = "0.1",
+    description="FunctionPlot is an application for plotting math functions",
+    long_description="""\
+FunctionPlot is an application for plotting 1-variable mathematical
+functions on a 2D graph. For every mathematical function it plots, it
+calculates respective points of interest and uses those to determine the
+optimal position and zoom scale for the graph.""",
+    author="George Vlahavas",
+    author_email="vlahavas ~ at ~ gmail ~ dot ~ com",
+    url="https://github.com/gapan/",
+    #package_dir = {'': 'src'}, # See packages below
+    package_data = {'': ['*.png']},
+    packages = find_packages(exclude="test"),
+    # Use this line if you've uncommented package_dir above.
+    #packages = find_packages("src", exclude="tests"),
+ 
+    entry_points = {
+        'console_scripts': ['functionplot = functionplot.functionplot.main:main']
+                    },
+ 
+    download_url = "https://github.com/gapan/functionplot",
+    zip_safe = True
+)
