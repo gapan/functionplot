c4e363bcc4baf213b360b8ce2ef533f22738b41a
George Vlahavas <vlahavas@gmail.com>
2015-09-14 23:24:31 +0300

    Add limit tests for trig functions
    Add limit tests for standard trigonometric functions:
    * sin(x)
    * cos(x)
    * tan(x)
    * cot(x)
    * sec(x)
    * csc(x)

--- test/test_limits.py
+++ test/test_limits.py
@@ -12,6 +12,54 @@ class TestLimits(unittest.TestCase):
     def test_default_limits(self):
         self.assertEqual(self.fg.get_limits(), ((-1.2, 1.2), (-1.2, 1.2)))
 
+    def test_sin(self):
+        self.fg.add_function('sin(x)')
+        (x_min, x_max), (y_min, y_max) = self.fg.get_limits()
+        self.assertTrue(-3 < y_min < -1.5 )
+        self.assertTrue(1.5 < y_max < 3)
+        # period is 2*pi and at least 2 periods should be shown
+        self.assertTrue(12.56 < x_max - x_min < 200)
+
+    def test_cos(self):
+        self.fg.add_function('cos(x)')
+        (x_min, x_max), (y_min, y_max) = self.fg.get_limits()
+        self.assertTrue(-3 < y_min < -1.5 )
+        self.assertTrue(1.5 < y_max < 3)
+        # period is 2*pi and at least 2 periods should be shown
+        self.assertTrue(12.56 < x_max - x_min < 200)
+
+    def test_tan(self):
+        self.fg.add_function('tan(x)')
+        (x_min, x_max), (y_min, y_max) = self.fg.get_limits()
+        self.assertTrue(-10 < y_min < -2 )
+        self.assertTrue(2 < y_max < 10)
+        # period is 2*pi and at least 2 periods should be shown
+        self.assertTrue(6.2 < x_max - x_min < 60)
+
+    def test_cot(self):
+        self.fg.add_function('cot(x)')
+        (x_min, x_max), (y_min, y_max) = self.fg.get_limits()
+        self.assertTrue(-10 < y_min < -2 )
+        self.assertTrue(2 < y_max < 10)
+        # period is 2*pi and at least 2 periods should be shown
+        self.assertTrue(6.2 < x_max - x_min < 60)
+
+    def test_sec(self):
+        self.fg.add_function('sec(x)')
+        (x_min, x_max), (y_min, y_max) = self.fg.get_limits()
+        self.assertTrue(-20 < y_min < -8 )
+        self.assertTrue(8 < y_max < 20)
+        # period is 2*pi and at least 2 periods should be shown
+        self.assertTrue(12.56 < x_max - x_min < 80)
+
+    def test_csc(self):
+        self.fg.add_function('csc(x)')
+        (x_min, x_max), (y_min, y_max) = self.fg.get_limits()
+        self.assertTrue(-20 < y_min < -8 )
+        self.assertTrue(8 < y_max < 20)
+        # period is 2*pi and at least 2 periods should be shown
+        self.assertTrue(12.56 < x_max - x_min < 80)
+
     def test_outlier_y(self):
         '''
         This test should find an outlier in the y axis and not take it into
